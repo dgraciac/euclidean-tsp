45e2c8f843540ff2f25962a9312e9b68e939f797
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-06-29 02:40:55 +0200

    change

--- src/main/kotlin/com/github/dgraciac/euclideantsp/ListExtensions.kt
+++ src/main/kotlin/com/github/dgraciac/euclideantsp/ListExtensions.kt
@@ -1,8 +1,11 @@
 package com.github.dgraciac.euclideantsp
 
+import java.security.InvalidAlgorithmParameterException
+
 fun <E> List<E>.permute(): List<List<E>> {
     return when (this.size) {
-        0, 1 -> listOf(this)
+        0 -> throw InvalidAlgorithmParameterException()
+        1 -> listOf(this)
         else -> this.map { element: E ->
             this.minusElement(element).permute().map { subPermutation: List<E> ->
                 listOf(element).plus(subPermutation)
