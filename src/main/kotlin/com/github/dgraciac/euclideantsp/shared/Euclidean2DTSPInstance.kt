bcd52b292a67eed523d231428aa810fdc7a0b593
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-07-03 11:03:17 +0200

    change

--- src/main/kotlin/com/github/dgraciac/euclideantsp/shared/Euclidean2DTSPInstance.kt
+++ src/main/kotlin/com/github/dgraciac/euclideantsp/shared/Euclidean2DTSPInstance.kt
@@ -1,7 +1,7 @@
 package com.github.dgraciac.euclideantsp.shared
 
 open class Euclidean2DTSPInstance(
-    val name: String = "unnamed",
+    val name: String,
     val points: List<Point>,
     val optimalLength: Double
 ) {
