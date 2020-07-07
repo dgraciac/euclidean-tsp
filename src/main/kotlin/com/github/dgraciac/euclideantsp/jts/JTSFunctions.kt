4b89179441105907d1e05ec15ccd7c0b01a936cb
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-07-07 12:08:35 +0200

    change

--- src/main/kotlin/com/github/dgraciac/euclideantsp/jts/JTSFunctions.kt
+++ src/main/kotlin/com/github/dgraciac/euclideantsp/jts/JTSFunctions.kt
@@ -36,6 +36,11 @@ internal fun List<Point>.listOfCoordinates(): List<Coordinate> = map { it.coordi
 internal fun Euclidean2DTSPInstance.centroid(): Point =
     ConvexHull(points.map { it.toCoordinate() }.toTypedArray(), GeometryFactory()).convexHull.centroid
 
+/**
+ * Find best insertion.
+ * Criteria: after inserting the point between each pair of consecutive points,
+ * retain the pair that minimizes distance of first->point->second
+ */
 internal fun ArrayList<Point>.findBestIndexToInsertAt(point: Point): Pair<Int, Double> {
     var bestIndexToInsertAt: Int = -1
     var minimumLength: Double = Double.POSITIVE_INFINITY
@@ -58,4 +63,34 @@ internal fun ArrayList<Point>.findBestIndexToInsertAt(point: Point): Pair<Int, D
 
     if (bestIndexToInsertAt == -1) throw RuntimeException("Best Index is null")
     return Pair(bestIndexToInsertAt, minimumLength)
+}
+
+/**
+ * Find best insertion.
+ * Criteria: after inserting the point between each pair of consecutive points,
+ * retain the pair that minimizes ratio (distance(first->point->second)/distance(first->second))
+ */
+internal fun ArrayList<Point>.findBestIndexToInsertAt2(point: Point): Pair<Int, Double> {
+    var bestIndexToInsertAt: Int = -1
+    var minimumRatio: Double = Double.POSITIVE_INFINITY
+
+    for (i: Int in 0 until this.size - 1) {
+        add(i + 1, point)
+        val isLinearRing: Boolean = isLinearRing().also { this.removeAt(i + 1) }
+        if (isLinearRing) {
+            val first: Point = this[i]
+            val second: Point = this[i + 1]
+            lengthAfterInsertBetweenPairOfPoints(first, second, point)
+                .let { length ->
+                    val ratio: Double = length / first.distance(second)
+                    if (ratio < minimumRatio) {
+                        bestIndexToInsertAt = i + 1
+                        minimumRatio = ratio
+                    }
+                }
+        }
+    }
+
+    if (bestIndexToInsertAt == -1) throw RuntimeException("Best Index is null")
+    return Pair(bestIndexToInsertAt, minimumRatio)
 }
--- src/test/kotlin/com/github/dgraciac/euclideantsp/TSPInstanceProvider.kt
+++ src/test/kotlin/com/github/dgraciac/euclideantsp/TSPInstanceProvider.kt
@@ -9,19 +9,19 @@ class TSPInstanceProvider : ArgumentsProvider {
     //TODO 4_B is failing
     override fun provideArguments(context: ExtensionContext): Stream<Arguments> {
         return Stream.of(
-//            Arguments.of(TRIVIAL),
-//            Arguments.of(_4_SQUARE),
-//            Arguments.of(_4_A),
-//            Arguments.of(_5_A),
-//            Arguments.of(_5_B),
-//            Arguments.of(_6_A),
-//            Arguments.of(_6_B),
-//            Arguments.of(_6_C),
+            Arguments.of(TRIVIAL),
+            Arguments.of(_4_SQUARE),
+            Arguments.of(_4_A),
+            Arguments.of(_5_A),
+            Arguments.of(_5_B),
+            Arguments.of(_6_A),
+            Arguments.of(_6_B),
+            Arguments.of(_6_C),
             Arguments.of(_6_D)
-//            ,
-//            Arguments.of(_10_A),
-//            Arguments.of(BERLIN_52),
-//            Arguments.of(ST_70)
+            ,
+            Arguments.of(_10_A),
+            Arguments.of(BERLIN_52),
+            Arguments.of(ST_70)
 //            ,
 //            Arguments.of(KRO_200),
 //            Arguments.of(A_280)
