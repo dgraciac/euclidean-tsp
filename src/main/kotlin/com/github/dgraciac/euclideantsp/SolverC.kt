b7a0986a3a796c8c95c9f29a66cc55f084a93cb0
David Gracia <david.gracia.celemendi@gmail.com>
2022-07-13 00:21:27 +0200

    change

---
+++ src/main/kotlin/com/github/dgraciac/euclideantsp/SolverC.kt
@@ -0,0 +1,16 @@
+package com.github.dgraciac.euclideantsp
+
+import com.github.dgraciac.euclideantsp.shared.Euclidean2DTSPInstance
+import com.github.dgraciac.euclideantsp.shared.Euclidean2DTSPSolver
+import com.github.dgraciac.euclideantsp.shared.Tour
+
+/**
+ * Calcular convex hull de los nodos no conectados restantes
+ * repetir hasta que no queden nodos
+ * Conectar los convex hulls para formar un tour
+ */
+class SolverC: Euclidean2DTSPSolver {
+    override fun compute(instance: Euclidean2DTSPInstance): Tour {
+        TODO("Not yet implemented")
+    }
+}
